90b377a974d43447aee9a7a59d32a0747a99c0ba
Sam <dowd95@gmail.com>
2016-04-14 20:09:52 -0500

    Added environment layout and set up MainActivity to at least show it

--- app/src/main/java/fr3380ot/com/gro2/MainActivity.java
+++ app/src/main/java/fr3380ot/com/gro2/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -90,24 +91,28 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_habits) {
-            // Handle the camera action
-        } else if (id == R.id.nav_environment) {
-
-        } else if (id == R.id.nav_rewards) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
+        switch(item.getItemId()){
+
+            case R.id.nav_habits:
+                // Handle the camera action
+                break;
+            case R.id.nav_environment:
+                setContentView(R.layout.environment);
+                break;
+            case R.id.nav_rewards:
+                break;
+            case R.id.nav_manage:
+                break;
+            case R.id.nav_share:
+                break;
+            case R.id.nav_send:
+                break;
+        }
+
         return true;
     }
 }
